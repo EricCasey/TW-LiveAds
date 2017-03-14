4c0f4b8b8fdd6acc72378fd494784a56a0b733d3
EricCasey <EricCasey509@gmail.com>
2017-03-13 22:43:39 -0400

    update

--- src/App.js
+++ src/App.js
@@ -12,28 +12,43 @@ class App extends Component {
   }
 
   render() {
-
     return (
       <div className="App">
 
         <div id="m300250">
           <div className="live">LIVE</div>
-          <div className="bottom">Visit Whistler Blackomb today!</div>
+          <div className="bottom1">Visit Whistler Blackomb Today!</div>
           <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/WhistlerBlackcomb_Logo.svg/1280px-WhistlerBlackcomb_Logo.svg.png" className="logo"/>
         </div>
         <div id="m160600">
           <div className="live">LIVE</div>
-          <div className="bottom"></div>
+          <div className="bottom2">
+
+            <br/>
+            All Lifts Open!
+            <br/>
+            17 Superpipes!
+            <br/>
+            99 Beers on the wall!
+            <br/>
+            10ft of powder!
+            <br/>
+            Billions of Snowflakes!
+          </div>
           <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/WhistlerBlackcomb_Logo.svg/1280px-WhistlerBlackcomb_Logo.svg.png" className="logo"/>
         </div>
         <div id="m72890">
           <div className="live">LIVE</div>
-          <div className="bottom"></div>
-          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/WhistlerBlackcomb_Logo.svg/1280px-WhistlerBlackcomb_Logo.svg.png" className="logo"/>
+          <div className="bottom3">
+            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/WhistlerBlackcomb_Logo.svg/1280px-WhistlerBlackcomb_Logo.svg.png" className="logo"/>
+            example
+          </div>
         </div>
         <div id="m180150">
           <div className="live">LIVE</div>
-          <div className="bottom"></div>
+          <div className="bottom4">
+            example
+          </div>
           <img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/65/WhistlerBlackcomb_Logo.svg/1280px-WhistlerBlackcomb_Logo.svg.png" className="logo"/>
         </div>
       </div>
